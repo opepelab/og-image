527ba96d8ee5b3cee6c61d1a53e21f11e4751fc5
sou <ocelot2828@gmail.com>
2022-03-18 21:25:34 +0900

    test

--- api/_lib/template.ts
+++ api/_lib/template.ts
@@ -97,19 +97,10 @@ function getCss(fontSize: string) {
         font-family: "Hind", "Noto Sans JP", sans-serif;
         font-size: ${sanitizeHtml(fontSize)};
         font-style: normal;
-        color: ${sanitizeHtml};
-        line-height: 1.2;
-        display: flex;
-        flex-direction: column;
-        width: 938px;
-        height: 600px;
-        top: 0;
-        bottom: 0;
-        left: 0;
-        right: 0;
-        margin: auto;
-        justify-content: center;
-        align-items: center;
+        color: black;
+        line-height: normal;
+        margin: 0;
+        text-align: center;
     }`;
 }
 
