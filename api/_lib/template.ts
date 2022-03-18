ca52c416401413c43943ce910c1f865573c89cb7
sou <ocelot2828@gmail.com>
2022-03-18 22:08:32 +0900

    test

--- api/_lib/template.ts
+++ api/_lib/template.ts
@@ -102,9 +102,8 @@ function getCss(fontSize: string) {
         padding: 50px;
         left:  0%;
         right: 0%;
-        top: 0;
-        bottom: 0;
-        margin: auto;
+        margin-top: auto;
+        margin-bottom: auto;
     }`;
 }
 
