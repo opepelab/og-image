6a24824ec38c55c5027eed3b9c10949c267041b3
sou <ocelot2828@gmail.com>
2022-03-19 12:31:38 +0900

    test

--- api/_lib/template.ts
+++ api/_lib/template.ts
@@ -101,8 +101,13 @@ function getCss(fontSize: string) {
         line-height: 1;
         padding-left: 50px;
         padding-right: 50px;
-        left:  0%;
-        right: 0%;
+        top: 0;
+        bottom: 0;
+        left: 0;
+        right: 0;
+        width: 100%;
+        height: 100%;
+        margin: auto;
     }`;
 }
 
